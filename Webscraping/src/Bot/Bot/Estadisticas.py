f33dbef6a856fcdd0afe6a2bc1995a6cb877d9ee
felinblackcat <47456740+felinblackcat@users.noreply.github.com>
2020-03-13 13:05:39 -0500

    update

---
+++ Webscraping/src/Bot/Bot/Estadisticas.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 12:36:53 2020
+
+@author: Acer
+"""
+
+import numpy as np
+import pandas as pd
+import pandas.io.sql as psql
+import psycopg2
+
+db_connection= psycopg2.connect(user = "postgres",password = "Felingato1992",host = "127.0.0.1",port = "5432",database = "tvrec")
+cursor = db_connection.cursor()
+
+sql_command = "SELECT * FROM {}.{};".format(str("public"), str("televisorbb"))
+data = pd.read_sql(sql_command, db_connection)
+print(data.head())
+print(data.count())
+data.describe()
+data[data.precio == 0]
+data[data.calificaciÓnbb == 0]['modelo']
+data["marca"].value_counts()
+data["resoluciÓn"].value_counts()
+data["tamaÑopantalla"].value_counts()
+data["tipodisplay"].value_counts()
+data["activo"].value_counts()
