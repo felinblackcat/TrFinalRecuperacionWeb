0e8844650e376e011ce394ca131981e79ab8de3f
felinblackcat <47456740+felinblackcat@users.noreply.github.com>
2020-03-27 22:39:35 -0500

    update

--- Webscraping/src/Recomendacion/views.py
+++ Webscraping/src/Recomendacion/views.py
@@ -1,24 +1,66 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
+import os
 from src.Bot.Bot import scraper
+import threading
 # Create your views here.
+import json
+
+
+def ejecutar_spyders():
+    scraper.startScan()
+    scraper.reactor.run()
+    data = {}
+    data["Estado"]="false"
+    with open('data.json', 'w') as file:json.dump(data, file, indent=4)
+
 @csrf_exempt
 def index (request):
     return render(request,'index.html')
 
 @csrf_exempt
 def runspider(request):
-     print("Respuesta",request)
-     #scraper.configure_logging()
-     #scraper.get_project_settings()
+    hilo  = threading.Thread(target=ejecutar_spyders)
+    if(os.path.isfile('data.json')):
+        
+        with open('data.json') as file:data = json.load(file)
+        if(data['Estado']=="true"):
+            
+            context = {
+                    'mensaje':'Error la araña ya se esta ejecutando intendalo mas tarde.',
+                } 
+            return render(request,'alltelevisores.html',context)
+        elif(data['Estado']=="false"):
+            
+            data = {}
+            data['Estado']="true"
+            with open('data.json', 'w') as file:json.dump(data, file, indent=4)
+            hilo.start()
+            context = {
+                'mensaje':'Inicio del proceso de scraping...',
+                }   
+            return render(request,'alltelevisores.html',context)
+        
+        
+        
+    else:
+        print("Entro")
+        data = {}
+        data['Estado']="true"
+        with open('data.json', 'w') as file:json.dump(data, file, indent=4)
+        hilo.start()
+        context = {
+                'mensaje':'Inicio del proceso de scraping...',
+            }  
      
      
-     #scraper.startScan()
-     #scraper.reactor.run()
-     context = {
-                'resultado':'ArañaEjecucion',
-            } 
-     return render(request,'alltelevisores.html',context)
+        return render(request,'alltelevisores.html',context)
+         
+         
+        
+         
+     
+         
  
-    
+ 
