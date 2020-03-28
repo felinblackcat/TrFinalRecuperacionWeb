244cfd14374ea5b2c8a36faffadc6a41bc51046e
felinblackcat <47456740+felinblackcat@users.noreply.github.com>
2020-03-28 17:28:52 -0500

    full registro

--- Webscraping/src/Recomendacion/views.py
+++ Webscraping/src/Recomendacion/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import viewsets
-
-# Create your views here.
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
+from django.contrib.auth import login, authenticate
 
 
 
@@ -13,7 +13,9 @@ def index (request):
     
     return render(request,'index.html')
 
-         
+@csrf_exempt  
+def login(request):
+    return render(request,'login.html')         
   
 @csrf_exempt   
 def registro(request):      
@@ -22,12 +24,41 @@ def registro(request):
 
 @csrf_exempt    
 def RegistrarUsuario(request):
-    print(request.POST.get('email'),request.POST.get('password'))
     
-    context = {
-                'mensaje':{'usuario':request.POST.get('email')},
-                }
     
-    return render(request,'index.html',context)        
+    if(request.method=="POST"):
+        
+        try:
+            Usuario = User.objects.create_user(request.POST.get('email'),request.POST.get('email'),request.POST.get('password'))
+            Usuario.save()
+            context = {
+                    'mensaje':{'usuario':request.POST.get('email'),'mensaje':'Registrado Correctamente.'},                    
+                    }
+    
+            return render(request,'index.html',context)      
+            
+            
+            
+        
+        except IntegrityError:
+            context = {
+                    'mensaje':{'usuario':request.POST.get('email'),'mensaje':'Error usuario ya registrado.'},                    
+                    }
+    
+            return render(request,'index.html',context)      
+            
+            
+          
  
- 
+def Loguearse(request):
+    user = authenticate(username=username, password=password)
+    login(request, user)
+    return redirect('home')
+
+    
+    
+    
+    
+    
+    
+    return render(request,'registro.html')
--- Webscraping/src/web/urls.py
+++ Webscraping/src/web/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from Recomendacion.views import index,registro,RegistrarUsuario
+from Recomendacion.views import index,registro,RegistrarUsuario,login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),    
     path('registro/',registro,name='registro'),
     path('RegistrarUsuario/',RegistrarUsuario,name='RegistrarUsuario'),
+    path('login/',login,name='login'),
 ]
