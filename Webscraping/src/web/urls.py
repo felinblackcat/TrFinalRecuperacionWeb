f8639ee9242051a4bb2f84a7a35f4c431c44383c
felinblackcat <47456740+felinblackcat@users.noreply.github.com>
2020-03-27 23:13:47 -0500

    update

--- Webscraping/src/web/urls.py
+++ Webscraping/src/web/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Recomendacion/', include('Recomendacion.urls')),
+    path('', include('Recomendacion.urls')),
+    #path('',include()),
 ]
