b59254b8a8085e5844aa1be77980de20692937ed
Luis Carlos Arrieta Sierra <lcarrietas@unal.edu.co>
2020-03-29 02:23:30 -0500

    Agregado lo que va del híbrido
    Falta en el híbrido integrar con lo que haga Chaves

--- Webscraping/src/Bot/Bot/Recomendacion.py
+++ Webscraping/src/Bot/Bot/Recomendacion.py
@@ -69,7 +69,9 @@ def conexion_bd(tabla):
             
     return df_tabla
         
-
+#****************************************************************************************************************
+#********** SISTEMA DE RECOMENDACION COLABORATIVO (USUARIO - USUARIO)********************************************
+#****************************************************************************************************************
 def colaborativo(usuario):
 
     df_calificacion = conexion_bd("calificacion")
@@ -91,7 +93,116 @@ def colaborativo(usuario):
     topuser = [modelo, calificacion for modelo, calificacion in top_n[usuario]]
         
     return topuser
+#********************************************************************************
+#************************* FIN COLABORATIVO *************************************
+#********************************************************************************
+
+
+#**********************************************************************************
+#************************** SISTEMA HIBRIDI ***************************************
+#**********************************************************************************
+
+#Funcion para calcular y modificar lso pesos d los sistemas basado en su precision
+def pesos():
+  #consultar la tabla precision para computar los aciertos de las recomendaciones
+  df_precision = conexion_bd("precision")
+ #Verificar que el dataframe que almacena la consulta no este vacio
+  if not df_precision.empty:
+    colabora = df_precision[df_precision['SISTEMA_RECOMENDACION'] == "Colaborativo"]
+    conten = df_precision[df_precision['SISTEMA_RECOMENDACION'] == "Contenido"]
+    lcola = colabora['CALIFICACION'].tolist()
+    lconte = conten['CALIFICACION'].tolist()
+
+    c,d = 0,0
+    for x in lcola:
+      if x == True:
+        c = c+1
+    for y in lconte:
+      if y == True:
+        d = d+1
+#calculo de los pesos, total positivos / total calificaciones, por cada RS independiente
+    if len(lcola) > 0:  
+      wcol = c/len(lcola)
+    else:
+      wcol = 1
+
+    if len(lconte) > 0:
+      wcont= d/len(lconte)
+    else:
+      wcont = 1
     
+    resultado = [wcol, wcont]
+ # en caso de estar vacio se asignan pesos de 1
+  else:
+    resultado = [1,1]
+  return resultado
+
+#funcion de recomendacion principal, la que se invoca para mostrar las recomendaciones
+def recomendacion (usuario):
+#invocar la función pesos
+  pesos = pesos()
+  wColaborativo = pesos[0]
+  wContenido = pesos[1]
+
+#*****obtener las recomendaciones de los sistemas de recomendacion independientes***
+# Obtener la lista que arroja el sistema colaborativo, invocandolo
+  topColaborativo = colaborativo(usuario)
+  topColaborativo = [[x[0], x[1], "Colaborativo"] for x in topColaborativo]
+  #listaColab = [[modelo, calificacion, "colaborativo"] for modelo, calificacion in topColaborativo
+  
+  topColaborativo = [[x[0],x[1]*wColaborativo, x[2]] for x in topColaborativo]
+
+  topContenido = contenido()
+  listaConte =[] # *****************No sé como lo saca Chaves **********************
+  #  multiplicarl la calificación por el peso del RS
+  listaConte = [[x[0],x[1]*wContenido, x[2]] for x in listaConte]
+
+#Fusionarlas y ordenarlas por calificación
+  listaB = topColaborativo + listaConte
+  listaB2 = listaB.sort(key= lambda cal : cal[1], reverse=True)
+#Eliminar modelos repetidos, conservando el de mayor calificación
+  listaHibrido = []
+  modelos =[]
+  for i in listaB2:
+    if i[0] not in modelos:
+      listaHibrido.append(i)
+      modelos.append(i[0])
+
+  #*************** Insertar las recomendaciones en la tabla Precision*************
+  try:
+    connection = psycopg2.connect(user="postgres",
+                                    password="Felingato1992",
+                                    host="127.0.0.1",
+                                    port="5432",
+                                    database="tvrec")
+    cursor = connection.cursor()
+
+    postgres_insert_query = """ INSERT INTO Precision ( MODELO , SISTEMA_RECOMENDACION ) VALUES (%s,%s)"""
+
+    for valores in listaHibrido:
+      insert_tuple = (valores[0], valores[2])
+      cursor.execute(postgres_insert_query, insert_tuple)
+
+      connection.commit()
+      count = cursor.rowcount
+
+  except (Exception, psycopg2.Error) as error :
+      if(connection):
+          print("Failed to insert record into mobile table", error)
+
+  finally:
+      #closing database connection.
+      if(connection):
+          cursor.close()
+          connection.close()
+          print("PostgreSQL connection is closed")
+  #*******************************************************************
+  # Regresar la lista de modelos recomendada
+  return modelos
+
+#***********************************************************************************
+#************************ FIN HIBRIDI **********************************************
+************************************************************************************
 
 """
 if __name__ == "__main__":
