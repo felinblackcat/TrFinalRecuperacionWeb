a47bdef184739b87cbedf8780fa0baa71bd7e67e
Luis Carlos Arrieta Sierra <lcarrietas@unal.edu.co>
2020-03-30 22:28:54 -0500

    Update Recomendacion.py

--- Webscraping/src/Bot/Bot/Recomendacion.py
+++ Webscraping/src/Bot/Bot/Recomendacion.py
@@ -75,42 +75,35 @@ def conexion_bd(tabla):
 
 #--------Funcion para calcular y modificar los pesos de los sistemas basados en la calificacion de los usuarios----------
 def pesos():
-  #consultar la tabla precision para computar los aciertos de las recomendaciones
+
   df_precision = conexion_bd("Precision")
- #Verificar que el dataframe que almacena la consulta no este vacio
   if not df_precision.empty:
- #Eliminar las filas de recomendaciones no calificadas por el usuario
-    df_precision = df_precision.dropna(subset=['CALIFICACION'])  
- #calcular una simple presicion de los sistemas individiales basados en las calificaciones positivas o negativas   
-    colabora = df_precision[df_precision['SISTEMA_RECOMENDACION'] == "Colaborativo"]
-    conten = df_precision[df_precision['SISTEMA_RECOMENDACION'] == "Contenido"]
-    lcola = colabora['CALIFICACION'].tolist()
-    lconte = conten['CALIFICACION'].tolist()
-
-    c,d = 0,0
-    for x in lcola:
-      if x == "True":
-        c = c+1
-    for y in lconte:
-      if y == "True":
-        d = d+1
-
-    if len(lcola) > 0:  
-      wcol = c/len(lcola)
-    else:
-      wcol = 1
-
-    if len(lconte) > 0:
-      wcont= d/len(lconte)
-    else:
-      wcont = 1
- # asignar los pesos para cada sistema que será la presicion calculada   
-    resultado = [wcol, wcont]
- # en caso de estar vacio se asignan pesos de 1
+     
+     df_precision = df_precision.dropna(subset=['CALIFICACION'])  
+     colabora = df_precision[df_precision['SISTEMA_RECOMENDACION'] == "Colaborativo"]
+     conten = df_precision[df_precision['SISTEMA_RECOMENDACION'] == "Contenido"]
+     lcola = colabora['CALIFICACION'].tolist()
+     lconte = conten['CALIFICACION'].tolist()
+     c,d = 0,0
+     for x in lcola:
+       if x == "True":
+         c = c+1
+     for y in lconte:
+       if y == "True":
+         d = d+1
+     if len(lcola) > 0:  
+       wcol = c/len(lcola)
+     else:
+       wcol = 1
+     if len(lconte) > 0:
+       wcont= d/len(lconte)
+     else:
+       wcont = 1
+     resultado = [wcol, wcont]
   else:
-    resultado = [1,1]
+     resultado = [1,1]
   return resultado
-#-------------------------------------------------------------------------------------------------------------------------
+ #-------------------------------------------------------------------------------------------------------------------------
 #-------Funcion Para el cambio de escala de los criterios de medicion de los RS y que tengan el mismo rango de valores----
 def cambio( x, oldMin, oldMax, newMin, newMax ):
   aux = (x-oldMin)*(newMax-newMin)/(oldMax-oldMin)
